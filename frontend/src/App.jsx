5415cb90dd3913a44be390d10cba765bcf46c275
Ivan Miguel Malabanan <48382903+Mewguel@users.noreply.github.com>
2024-05-30 22:25:48 +0800

    [Update] Added Tailwind CSS and Navbar component (#5)
    [Chore] Cleanup backend cache gitignored pycache

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,43 @@
-import React from "react"
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
-import ProtectedRoute from "./components/ProtectedRoute"
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import Navbar from "./components/Navbar";
 
 const Logout = () => {
-  localStorage.clear()
-  return <Navigate to="/login"/>
-}
+  localStorage.clear();
+  return <Navigate to="/login" />;
+};
 
 const RegisterAndLogout = () => {
-  localStorage.clear()
-  return <Register />
-}
+  localStorage.clear();
+  return <Register />;
+};
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/"
-        element = {
-          <ProtectedRoute>
-              <Home/>
-          </ProtectedRoute>
-        }
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
         />
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/logout" element={<Logout/>}/>
-        <Route path="/register" element={<RegisterAndLogout/>}/>
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,64 +3,67 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 
-import "../styles/Form.css"
+import "../styles/Form.css";
+import logo from "../assets/logo.jpg";
 
 import LoadingIndicator from "./LoadingIndicator";
 
-const Form = ({route, method}) => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
+const Form = ({ route, method }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const name = method === "login" ? "Login" : "Register"; 
+  const name = method === "login" ? "Login" : "Register";
 
-    const handleSubmit = async (e) => {
-        setLoading(true);
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
 
-        try {
-            const res = await api.post(route, {username, password});
-            if(method === "login"){
-                localStorage.setItem(ACCESS_TOKEN, res.data.access);
-                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-                navigate("/")
-            }
-            else{
-                navigate("/login");
-            }
-        } catch (error) {
-            alert(error);
-        }
-        finally{
-            setLoading(false);
-        }
+    try {
+      const res = await api.post(route, { username, password });
+      if (method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return <form onSubmit={handleSubmit} className="form-container">
-        <h1>{name}</h1>
-        <input
-            className="form-input"
-            type="text"
-            value={username}
-            onChange={ (e) => {setUsername(e.target.value)}}
-            placeholder="Username"
-        />
-        <input
-            className="form-input"
-            type="password"
-            value={password}
-            onChange={ (e) => {setPassword(e.target.value)}}
-            placeholder="Password"
-        />
-        {loading && <LoadingIndicator/>}
-        <button className="form-button" type="submit">
-            {name}
-
-        </button>   
-
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <img className="h-20 m-5" src={logo} alt="Centennial Waters Logo" />
+      <h1 className="font-semibold text-2xl">{name}</h1>
+      <input
+        className="form-input"
+        type="text"
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+        placeholder="Username"
+      />
+      <input
+        className="form-input"
+        type="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        placeholder="Password"
+      />
+      {loading && <LoadingIndicator />}
+      <button className="form-button" type="submit">
+        {name}
+      </button>
     </form>
+  );
+};
 
-}
-
-export default Form
+export default Form;
