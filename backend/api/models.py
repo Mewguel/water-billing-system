9ffccfbd39f21e487fa20509ff75b4e23f830ff3
Ivan Miguel Malabanan <meguelvan@gmail.com>
2024-06-03 00:05:46 +0800

    [Update] Changed Logo and Favicon
    [Update] Initial Database migration for bill
    
    [Update] Created Bill Initial UI Layout Grid

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Bill(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    account_holder = models.CharField(max_length=100)
+    account_number = models.CharField(max_length=10, default="00-00-0000")
+    customer_address = models.CharField(max_length=120, default="")
     created_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
