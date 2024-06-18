68309486123afe2611d3ff1a942364dd67fc93e1
Ivan Miguel Malabanan <48382903+Mewguel@users.noreply.github.com>
2024-06-19 05:46:49 +0800

    [Update] Frontend api backend connection (#16)

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/waterbilling/backend-xcf/v1";
+const apiUrl = "/choreo-apis/waterbilling/backend/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
