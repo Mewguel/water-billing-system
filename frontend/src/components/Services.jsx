29aabfddf87aa980313349bd34efed3b00a7575b
Ivan Miguel Malabanan <meguelvan@gmail.com>
2024-06-25 20:14:32 +0800

    [Added] Admin Page Layout and Services updates

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import advisory from "../assets/advisory.png";
 import invoice from "../assets/invoice.png";
 import warning from "../assets/warning.png";
@@ -6,27 +8,44 @@ import feedback from "../assets/feedback.png";
 import payment from "../assets/payment.png";
 
 const Services = () => {
+  const navigate = useNavigate();
   return (
     <div className="card container flex flex-col items-center m-5 rounded">
       <h2 className="text-4xl font-bold m-5">Services</h2>
       <div className="services container flex flex-row items-center justify-center gap-10   ">
-        <div className="flex flex-col items-center bg-yellow-300 rounded-md">
+        <div
+          className="flex flex-col items-center bg-yellow-300 rounded-md cursor-pointer"
+          onClick={() => navigate("/advisory")}
+        >
           <img className="w-28 h-28 p-2" src={advisory} alt="advisory"></img>
           <span className="text-lg font-medium"> Advisory </span>
         </div>
-        <div className="flex flex-col items-center bg-yellow-300 rounded-md">
+
+        <div
+          className="flex flex-col items-center bg-yellow-300 rounded-md cursor-pointer"
+          onClick={() => navigate("/manageBills")}
+        >
           <img className="w-28 h-28 p-2" src={invoice} alt="invoice"></img>
           <span className="text-lg font-medium"> Invoice </span>
         </div>
-        <div className="flex flex-col items-center bg-yellow-300 rounded-md">
+        <div
+          className="flex flex-col items-center bg-yellow-300 rounded-md cursor-pointer"
+          onClick={() => navigate("/notice")}
+        >
           <img className="w-28 h-28 p-2" src={warning} alt="warning"></img>
           <span className="text-lg font-medium"> Notice </span>
         </div>
-        <div className="flex flex-col items-center bg-yellow-300 rounded-md">
+        <div
+          className="flex flex-col items-center bg-yellow-300 rounded-md cursor-pointer"
+          onClick={() => navigate("/feedback")}
+        >
           <img className="w-28 h-28 p-2" src={feedback} alt="feedback"></img>
           <span className="text-lg font-medium"> Feedback </span>
         </div>
-        <div className="flex flex-col items-center bg-yellow-300 rounded-md">
+        <div
+          className="flex flex-col items-center bg-yellow-300 rounded-md cursor-pointer"
+          onClick={() => navigate("/payment")}
+        >
           <img className="w-28 h-28 p-2" src={payment} alt="payment"></img>
           <span className="text-lg font-medium"> Payment </span>
         </div>
--- frontend/src/pages/Admin/ManageUser.jsx
+++ frontend/src/pages/Admin/ManageUser.jsx
@@ -5,82 +5,93 @@ import Table from "../../components/Table";
 const users = [
   {
     id: 1,
-    name: "Electronics",
-    email: "Apple",
-    username: "iPhone 13",
-    address: "The latest iPhone with advanced features",
+    name: "Ibanez, Marlon C.",
+    email: "ibanez@gmail.com",
+    username: "marlonIbanez",
+    address: "Blk39 L1 Southville Subd. Timbao",
   },
   {
     id: 2,
-    name: "Clothing",
-    email: "Nike",
-    username: "Running Shoes",
-    address: "High-quality running shoes for athletes",
+    name: "Purificacion, Nora S.",
+    email: "purificacion@gmail.com",
+    username: "puriNora",
+    address: "Blk1 L9 St. Joseph Village",
   },
   {
     id: 3,
-    name: "Books",
-    email: "Penguin Books",
-    username: "The Great Gatsby",
-    address: "Classic novel by F. Scott Fitzgerald",
+    name: "Villanueva, Marlon",
+    email: "villanuevaMarlon@gmail.com",
+    username: "villMarlon",
+    address: "Blk16 L11 Timbao",
   },
   {
     id: 4,
-    name: "Home Appliances",
-    email: "Samsung",
-    username: "Smart Refrigerator",
-    address: "Refrigerator with smart features and spacious design",
+    name: "Edano, Fernando",
+    email: "edano@gmail.com",
+    username: "edanoFernando",
+    address: "Blk74 L11 La Solidaridad-2A",
   },
 ];
 
-const userColumns = [
-  { header: "ID", accessor: "id" },
-  { header: "Name", accessor: "name" },
-  { header: "Email", accessor: "email" },
-  {
-    header: "Username",
-    accessor: "username",
-  },
-  {
-    header: "Address",
-    accessor: "address",
-  },
-  {
-    header: "Actions",
-    accessor: "actions",
-    render: () => (
-      <>
-        <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300
+const ManageUser = () => {
+  const userColumns = [
+    { header: "ID", accessor: "id" },
+    { header: "Name", accessor: "name" },
+    { header: "Email", accessor: "email" },
+    {
+      header: "Username",
+      accessor: "username",
+    },
+    {
+      header: "Address",
+      accessor: "address",
+    },
+    {
+      header: "Actions",
+      accessor: "actions",
+      render: () => (
+        <>
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300
               font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 
               dark:bg-blue-600 dark:hover:bg-blue-700
               focus:outline-none dark:focus:ring-blue-800"
-        >
-          Update
-        </button>
-        <button
-          type="button"
-          className="focus:outline-none rounded-lg
+            onClick={openModal}
+          >
+            Update
+          </button>
+          <button
+            type="button"
+            className="focus:outline-none rounded-lg
               text-white bg-red-700 hover:bg-red-800
               dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900
               focus:ring-4 focus:ring-red-300 
               font-medium text-sm 
               px-5 py-2.5 me-2 mb-2"
-        >
-          Delete
-        </button>
-      </>
-    ),
-  },
-];
+          >
+            Delete
+          </button>
+        </>
+      ),
+    },
+  ];
 
-const ManageUser = () => {
   const [userData, setUserData] = useState([]);
 
   useEffect(() => {
     setUserData(users);
   }, []);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <>
@@ -88,6 +99,37 @@ const ManageUser = () => {
         <h1 className="text-xl sm:text-2xl font-medium mb-2">Manage Users</h1>
       </div>
       <Table data={userData} columns={userColumns} />
+
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+          <div className="bg-white rounded-lg shadow-lg p-6">
+            <h2 className="text-2xl font-bold mb-4">Update User</h2>
+            <div className="flex flex-row">
+              <p className="mb-4">Name:</p>
+              <input type="text" placeholder="Name" className="mb-4 ml-2" />
+            </div>
+            <div className="flex flex-row">
+              <p className="mb-4">Email:</p>
+              <input type="text" placeholder="Email" className="mb-4 ml-2" />
+            </div>
+            <div className="flex flex-row">
+              <p className="mb-4">Address:</p>
+              <input type="text" placeholder="Address" className="mb-4 ml-2" />
+            </div>
+
+            <button
+              type="button"
+              className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300
+                font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 
+                dark:bg-red-500 dark:hover:bg-red-600
+                focus:outline-none dark:focus:ring-red-700"
+              onClick={closeModal}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
