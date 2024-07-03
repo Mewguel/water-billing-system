9da345f6d8b5523cc8497c9223097f17a1bc00dc
Ivan Miguel Malabanan <meguelvan@gmail.com>
2024-07-03 23:03:58 +0800

    [Update] Superuser

--- backend/custom_user/models.py
+++ backend/custom_user/models.py
@@ -6,10 +6,14 @@ class User(BaseUser):
     firstname = models.CharField(max_length=50, default="default_firstname")
     lastname = models.CharField(max_length=30, default="default_lastname")
     username = models.CharField(max_length=20,
-                                unique=True,
-                                default="default_username")
+                                unique=True, blank=True, null=True)
     address = models.CharField(max_length=255, blank=True, null=True)
     objects = BaseUserManager()
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.email.split('@')[0]
+        super(User, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.email
